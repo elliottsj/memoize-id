94a1a8c2f861632efe462c14cdf36a4b6b127fc7
Spencer Elliott <me@elliottsj.com>
2016-02-20 17:04:40 -0500

    Add the package & code

---
+++ __tests__/memoize-test.js
@@ -0,0 +1,118 @@
+jest.dontMock('../index');
+const memoize = require('../index').default;
+
+describe('memoize', () => {
+  it('memoizes when no arguments are passed', () => {
+    let i = 0;
+    function fn() {
+      i += 1;
+      return i;
+    }
+    const memoizedFn = memoize(fn);
+    expect(i).toBe(0);
+    expect(memoizedFn()).toBe(1);
+    expect(i).toBe(1);
+    expect(memoizedFn()).toBe(1);
+  });
+
+  it('memoizes when one argument is passed', () => {
+    let i = 0;
+    function fn(foo) {
+      i += 1;
+      return {
+        arg: foo,
+        i,
+      };
+    }
+    const memoizedFn = memoize(fn);
+    expect(i).toBe(0);
+    expect(memoizedFn()).toEqual({ arg: undefined, i: 1 });
+    expect(i).toBe(1);
+    expect(memoizedFn()).toEqual({ arg: undefined, i: 1 });
+    expect(i).toBe(1);
+    expect(memoizedFn('foo')).toEqual({ arg: 'foo', i: 2 });
+    expect(i).toBe(2);
+    expect(memoizedFn('foo')).toEqual({ arg: 'foo', i: 2 });
+    expect(i).toBe(2);
+  });
+
+  it('memoizes when three arguments are passed', () => {
+    let i = 0;
+    function fn(foo, bar, baz) {
+      i += 1;
+      return {
+        args: [foo, bar, baz],
+        i,
+      };
+    }
+    const memoizedFn = memoize(fn);
+    expect(i).toBe(0);
+    expect(memoizedFn()).toEqual({ args: [undefined, undefined, undefined], i: 1 });
+    expect(i).toBe(1);
+    expect(memoizedFn()).toEqual({ args: [undefined, undefined, undefined], i: 1 });
+    expect(i).toBe(1);
+    expect(memoizedFn('foo', 'bar', 'baz')).toEqual({ args: ['foo', 'bar', 'baz'], i: 2 });
+    expect(i).toBe(2);
+    expect(memoizedFn('foo', 'bar', 'baz')).toEqual({ args: ['foo', 'bar', 'baz'], i: 2 });
+    expect(i).toBe(2);
+  });
+
+  it('memoizes when three object arguments are passed', () => {
+    let i = 0;
+    function fn(foo, bar, baz) {
+      i += 1;
+      return {
+        args: [foo, bar, baz],
+        i,
+      };
+    }
+    const memoizedFn = memoize(fn);
+    expect(i).toBe(0);
+    expect(memoizedFn()).toEqual({ args: [undefined, undefined, undefined], i: 1 });
+    expect(i).toBe(1);
+    expect(memoizedFn()).toEqual({ args: [undefined, undefined, undefined], i: 1 });
+    expect(i).toBe(1);
+    const args = [{ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' }];
+    expect(
+      memoizedFn(...args)
+    ).toEqual(
+      { args: [{ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' }], i: 2 }
+    );
+    expect(i).toBe(2);
+    expect(
+      memoizedFn(...args)
+    ).toEqual(
+      { args: [{ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' }], i: 2 }
+    );
+    expect(i).toBe(2);
+  });
+
+  it('does not memoize when non-identical arguments are passed', () => {
+    let i = 0;
+    function fn(foo, bar, baz) {
+      i += 1;
+      return {
+        args: [foo, bar, baz],
+        i,
+      };
+    }
+    const memoizedFn = memoize(fn);
+    expect(i).toBe(0);
+    expect(memoizedFn()).toEqual({ args: [undefined, undefined, undefined], i: 1 });
+    expect(i).toBe(1);
+    expect(memoizedFn()).toEqual({ args: [undefined, undefined, undefined], i: 1 });
+    expect(i).toBe(1);
+    expect(
+      memoizedFn({ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' })
+    ).toEqual(
+      { args: [{ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' }], i: 2 }
+    );
+    expect(i).toBe(2);
+    expect(
+      memoizedFn({ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' })
+    ).toEqual(
+      { args: [{ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' }], i: 3 }
+    );
+    expect(i).toBe(3);
+  });
+});
---
+++ index.js
@@ -0,0 +1,57 @@
+const sentinel = Symbol(
+  'Cache key for `memoize` corresponding to the cached value for a complete set of arguments'
+);
+
+/**
+ * Get a value from the given cache Map, accessing an inner Map for each value in `keys`.
+ * If a value does not exist, return undefined.
+ * @param  {Map}        cache The base Map from which to get a value
+ * @param  {Array<Any>} keys  An array of keys for which nested Maps will be accessed
+ * @return {Any}        The value itself
+ */
+function get(cache, keys) {
+  if (keys.length === 0) {
+    return cache.get(sentinel);
+  }
+  const [key, ...restKeys] = keys;
+  const nestedCache = cache.get(key);
+  if (!nestedCache) {
+    return undefined;
+  }
+  return get(nestedCache, restKeys);
+}
+
+/**
+ * Set a value in the given cache Map, creating a new nested Map
+ * (or using an existing Map) for each value in `keys`.
+ * The innermost Map will have `map.get(sentinel) === value`.
+ * @param {Map}        cache The base Map for which to set a value
+ * @param {Array<Any>} keys  An array of keys for which new nested Maps will be created/used
+ * @param {Any}        value The value to set in the cache
+ */
+function set(cache, keys, value) {
+  if (keys.length === 0) {
+    cache.set(sentinel, value);
+  } else {
+    const [key, ...restKeys] = keys;
+    let nestedCache = cache.get(key);
+    if (!nestedCache) {
+      nestedCache = new Map();
+      cache.set(key, nestedCache);
+    }
+    set(nestedCache, restKeys, value);
+  }
+  return value;
+}
+
+/**
+ * Memoize the given function by the identity of its arguments.
+ * @param  {Function} fn The function to memoize
+ * @return {Function}    The memoized function
+ */
+export default function memoize(fn) {
+  const cache = new Map();
+  return function memoized(...args) {
+    return get(cache, args) || set(cache, args, fn(...args));
+  };
+}
