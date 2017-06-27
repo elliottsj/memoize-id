4162476176423d138b235d8676b1426b820e28fe
Spencer Elliott <me@elliottsj.com>
2017-06-27 15:06:14 -0400

    Use prettier

--- src/__tests__/memoize-test.js
+++ src/__tests__/memoize-test.js
@@ -47,15 +47,30 @@ describe('memoize', () => {
     }
     const memoizedFn = memoize(fn);
     expect(i).toBe(0);
-    expect(memoizedFn()).toEqual({ args: [undefined, undefined, undefined], i: 1 });
+    expect(memoizedFn()).toEqual({
+      args: [undefined, undefined, undefined],
+      i: 1,
+    });
     expect(i).toBe(1);
-    expect(memoizedFn()).toEqual({ args: [undefined, undefined, undefined], i: 1 });
+    expect(memoizedFn()).toEqual({
+      args: [undefined, undefined, undefined],
+      i: 1,
+    });
     expect(i).toBe(1);
-    expect(memoizedFn('foo', 'bar', 'baz')).toEqual({ args: ['foo', 'bar', 'baz'], i: 2 });
+    expect(memoizedFn('foo', 'bar', 'baz')).toEqual({
+      args: ['foo', 'bar', 'baz'],
+      i: 2,
+    });
     expect(i).toBe(2);
-    expect(memoizedFn('foo', 'bar', 'baz')).toEqual({ args: ['foo', 'bar', 'baz'], i: 2 });
+    expect(memoizedFn('foo', 'bar', 'baz')).toEqual({
+      args: ['foo', 'bar', 'baz'],
+      i: 2,
+    });
     expect(i).toBe(2);
-    expect(memoizedFn('foo', 'qux', 'baz')).toEqual({ args: ['foo', 'qux', 'baz'], i: 3 });
+    expect(memoizedFn('foo', 'qux', 'baz')).toEqual({
+      args: ['foo', 'qux', 'baz'],
+      i: 3,
+    });
     expect(i).toBe(3);
   });
 
@@ -70,22 +85,26 @@ describe('memoize', () => {
     }
     const memoizedFn = memoize(fn);
     expect(i).toBe(0);
-    expect(memoizedFn()).toEqual({ args: [undefined, undefined, undefined], i: 1 });
+    expect(memoizedFn()).toEqual({
+      args: [undefined, undefined, undefined],
+      i: 1,
+    });
     expect(i).toBe(1);
-    expect(memoizedFn()).toEqual({ args: [undefined, undefined, undefined], i: 1 });
+    expect(memoizedFn()).toEqual({
+      args: [undefined, undefined, undefined],
+      i: 1,
+    });
     expect(i).toBe(1);
     const args = [{ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' }];
-    expect(
-      memoizedFn(...args)
-    ).toEqual(
-      { args: [{ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' }], i: 2 }
-    );
+    expect(memoizedFn(...args)).toEqual({
+      args: [{ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' }],
+      i: 2,
+    });
     expect(i).toBe(2);
-    expect(
-      memoizedFn(...args)
-    ).toEqual(
-      { args: [{ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' }], i: 2 }
-    );
+    expect(memoizedFn(...args)).toEqual({
+      args: [{ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' }],
+      i: 2,
+    });
     expect(i).toBe(2);
   });
 
@@ -100,21 +119,25 @@ describe('memoize', () => {
     }
     const memoizedFn = memoize(fn);
     expect(i).toBe(0);
-    expect(memoizedFn()).toEqual({ args: [undefined, undefined, undefined], i: 1 });
+    expect(memoizedFn()).toEqual({
+      args: [undefined, undefined, undefined],
+      i: 1,
+    });
     expect(i).toBe(1);
-    expect(memoizedFn()).toEqual({ args: [undefined, undefined, undefined], i: 1 });
+    expect(memoizedFn()).toEqual({
+      args: [undefined, undefined, undefined],
+      i: 1,
+    });
     expect(i).toBe(1);
-    expect(
-      memoizedFn({ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' })
-    ).toEqual(
-      { args: [{ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' }], i: 2 }
-    );
+    expect(memoizedFn({ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' })).toEqual({
+      args: [{ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' }],
+      i: 2,
+    });
     expect(i).toBe(2);
-    expect(
-      memoizedFn({ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' })
-    ).toEqual(
-      { args: [{ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' }], i: 3 }
-    );
+    expect(memoizedFn({ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' })).toEqual({
+      args: [{ foo: 'oof' }, { bar: 'rab' }, { baz: 'zab' }],
+      i: 3,
+    });
     expect(i).toBe(3);
   });
 
@@ -128,8 +151,14 @@ describe('memoize', () => {
       };
     }
     const memoizedFn = memoize(fn, { arity: 2 });
-    expect(memoizedFn('foo', 'bar', 'baz')).toEqual({ args: ['foo', 'bar', 'baz'], i: 1 });
-    expect(memoizedFn('foo', 'bar', 'qux')).toEqual({ args: ['foo', 'bar', 'baz'], i: 1 });
+    expect(memoizedFn('foo', 'bar', 'baz')).toEqual({
+      args: ['foo', 'bar', 'baz'],
+      i: 1,
+    });
+    expect(memoizedFn('foo', 'bar', 'qux')).toEqual({
+      args: ['foo', 'bar', 'baz'],
+      i: 1,
+    });
   });
 
   it('memoizes the initial return value when and arity of 0 is passed', () => {
@@ -142,11 +171,14 @@ describe('memoize', () => {
       };
     }
     const memoizedFn = memoize(fn, { arity: 0 });
-    expect(memoizedFn('foo', 'bar', 'baz')).toEqual({ args: ['foo', 'bar', 'baz'], i: 1 });
+    expect(memoizedFn('foo', 'bar', 'baz')).toEqual({
+      args: ['foo', 'bar', 'baz'],
+      i: 1,
+    });
     expect(memoizedFn('qux')).toEqual({ args: ['foo', 'bar', 'baz'], i: 1 });
   });
 
-  pit('memoizes node-style async functions', async () => {
+  it('memoizes node-style async functions', async () => {
     let i = 0;
     function fn(foo, bar, callback) {
       i += 1;
@@ -158,13 +190,13 @@ describe('memoize', () => {
       });
     }
     const memoizedFn = memoize(fn, { async: true });
-    await new Promise((resolve) => {
+    await new Promise(resolve => {
       memoizedFn('foo', 'bar', (error, result) => {
         expect(result).toEqual({ args: ['foo', 'bar'], i: 1 });
         resolve();
       });
     });
-    await new Promise((resolve) => {
+    await new Promise(resolve => {
       let sync = true;
       memoizedFn('foo', 'bar', (error, result) => {
         expect(result).toEqual({ args: ['foo', 'bar'], i: 1 });
@@ -175,7 +207,7 @@ describe('memoize', () => {
     });
   });
 
-  pit('calls the callback immediately when using async: \'immediate\'', async () => {
+  it("calls the callback immediately when using async: 'immediate'", async () => {
     let i = 0;
     function fn(foo, bar, callback) {
       i += 1;
@@ -187,13 +219,13 @@ describe('memoize', () => {
       });
     }
     const memoizedFn = memoize(fn, { async: 'immediate' });
-    await new Promise((resolve) => {
+    await new Promise(resolve => {
       memoizedFn('foo', 'bar', (error, result) => {
         expect(result).toEqual({ args: ['foo', 'bar'], i: 1 });
         resolve();
       });
     });
-    await new Promise((resolve) => {
+    await new Promise(resolve => {
       let sync = true;
       memoizedFn('foo', 'bar', (error, result) => {
         expect(result).toEqual({ args: ['foo', 'bar'], i: 1 });
